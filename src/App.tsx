d50ac85241de9a04990ef50bde9704ce9554b4d3
kritagya-khanna <kritagykhanna21k@gmail.com>
2024-10-20 22:30:01 +0530

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Navbar from './Navbar.tsx'
+import SocialMedia from './SocialMedia.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wraper">
+      <Navbar/>
+      <div className="container">
+        <div className="left">
+          <p className="hello">HELLO, MY NAME IS</p>
+          <h1 className="name">KRITAGYA <br /> KHANNA</h1>
+          <p className="about"> I'M AN ENGINEERING STUDENT <br /> MAJORING IN INFORMATION TECHNOLOGY</p>
+          <SocialMedia/>
+        </div>
+        
+        <div className="right">
+          <div className="image">
+            <img src="src\assets\me-removebg.png" alt="" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+    <hr />
+
     </>
   )
 }
---
+++ src/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <div className="nav-container">
+            <div className="kk-logo"></div>
+            <nav className="navigator">
+                <ul className="navi">
+                    <li>HOME</li>
+                    <li>ABOUT ME</li>
+                    <li>MY WORK</li>
+                    <li>CONTACT ME</li>
+                </ul>
+            </nav>
+            <div className="theme">A</div>
+        </div>
+    );
+};
+
+export default Navbar;
